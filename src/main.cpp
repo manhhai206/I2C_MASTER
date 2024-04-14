c499f04785ad5130cd18b8c0d64ec6077d645100
Manh Hai <manhtronghai2062002@gmail.com>
2024-04-14 19:21:36 +0700

    Your commit message

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,12 @@
 #define FULL (HALF * 2)
 
 void I2C_Master_Start();
+byte I2C_Master_ReadByte();
+void I2C_Master_SendACK();
 void I2C_Master_Stop();
-uint8_t I2C_Master_ReadACK();
 void I2C_Master_WriteByte(byte data);
-int8_t I2C_Master_WriteData(byte address, char *data);
+uint8_t I2C_Master_ReadACK();
+int8_t I2C_Master_ReadData(byte address, byte *data,byte count);
 
 void setup(){
   delay(100);
@@ -24,27 +26,37 @@ void setup(){
 }
 
 void loop() {
-  int8_t result;
-  char data[]="abcd";
-  result = I2C_Master_WriteData(0X55,data);
-  if(result==1)   Serial.println("Master want to write data");
-  else if(result==-1) Serial.println("Master write fail");
+  byte rev[10]={0};
+  int8_t res;
+  res = I2C_Master_ReadData(0x55,rev,4);
+  if(res==1)
+  {
+    Serial.println("read oke");
+    Serial.println(String((char*)rev));
+  }
+  else
+  {
+    Serial.println("fail to read");
+  }
   delay(100);
 }
 
-void I2C_Master_Start() {
+void I2C_Master_Start()
+{
   SDA_HIGH;
   SCL_HIGH;
   SDA_OUTPUT;
   SCL_OUTPUT;
-  delayMicroseconds(FULL);
+  delayMicroseconds(HALF);
   SDA_LOW;
   delayMicroseconds(HALF);
   SCL_LOW;
   delayMicroseconds(HALF);
 }
 
-void I2C_Master_Stop() {
+void I2C_Master_Stop()
+{
+  SCL_LOW;
   SDA_OUTPUT;
   SDA_LOW;
   delayMicroseconds(HALF);
@@ -67,6 +79,23 @@ void I2C_Master_WriteByte(byte data) {
   }
 }
 
+byte I2C_Master_ReadByte()
+{
+  byte data=0;
+  SDA_INPUT;
+  SCL_OUTPUT;
+  SCL_LOW;
+  for(int i=0;i<8;i++)
+    {
+      delayMicroseconds(HALF);
+      SCL_HIGH;
+      data = (data<<1) | SDA_READ;
+      delayMicroseconds(HALF);
+      SCL_LOW;
+    }
+    return data;
+}
+
 uint8_t I2C_Master_ReadACK() {
   uint8_t ACK;
   SDA_INPUT;
@@ -79,25 +108,39 @@ uint8_t I2C_Master_ReadACK() {
   return ACK;
 }
 
-int8_t I2C_Master_WriteData(byte address, char *data) { 
+void I2C_Master_SendACK()
+{
+  SCL_LOW;
+  SDA_OUTPUT;
+  SDA_LOW; //SEND ACK
+  delayMicroseconds(HALF);
+  SCL_HIGH;
+  delayMicroseconds(HALF);
+  SCL_LOW;
+  SDA_INPUT;
+  delayMicroseconds(HALF);
+}
+
+int8_t I2C_Master_ReadData(byte address, byte *data,byte count)
+{
   int8_t ACK;
   I2C_Master_Start();
-  address = address << 1;
+  address = (address << 1) + 1;
   I2C_Master_WriteByte(address);
-  ACK = I2C_Master_ReadACK();
-  if (ACK == 1) {
+  ACK=I2C_Master_ReadACK();
+  if(ACK==1)
+  {
     I2C_Master_Stop();
     return -1;
   }
-  while (*data != '\0') {
-    I2C_Master_WriteByte(*data);
-    ACK = I2C_Master_ReadACK();
-    if (ACK == 1) {
-      I2C_Master_Stop();
-      return -1;
-    }
+  for(int i=0;i<count;i++)
+  {
+    *data = I2C_Master_ReadByte();
     data++;
+    I2C_Master_SendACK();
   }
   I2C_Master_Stop();
-  return 1;
 }
+
+
+
